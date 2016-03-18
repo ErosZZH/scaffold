dc53940b7ebb3c76af4c995346033ad2d958e3ec
rick <rick@yzlpie.com>
2016-03-18 13:32:41 +0800

    add rz-hbase

---
+++ rz-hbase/src/main/java/com/rick/scaffold/App.java
@@ -0,0 +1,13 @@
+package com.rick.scaffold;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
